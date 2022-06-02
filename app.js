182fabfda2334989fc8e43e6e9ef3479afa51385
duaneywadey <dequitoduaney@gmail.com>
2022-06-02 12:01:06 +0800

    added pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const celsius = document.getElementById('celsius');
 const fahrenheit = document.getElementById('fahrenheit');
+const kelvin = document.getElementById('kelvin');
 
 function celToFar() {
 	let output = ( parseFloat(celsius.value) * 9/5) + 32;
@@ -12,4 +13,35 @@ function farToCel() {
 	let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
 	celsius.value = parseFloat(output.toFixed(2));
 	console.log(output + "°C");
+}
+
+
+// New vals
+
+function KelToCel() {
+	let output = ( parseFloat(kelvin.value) - 273.15);
+	celsius.value = parseFloat(output.toFixed(2));
+	console.log(output + "°C");
+
+}
+
+function CelToKel() {
+	let output = ( parseFloat(celsius.value) + 273.15);
+	kelvin.value = parseFloat(output.toFixed(2));
+	console.log(output + " K");
+}
+
+// (0K − 273.15) × 9/5 + 32
+
+function KelToFar() {
+	let output = ( parseFloat(kelvin.value) - 273.15) * 9/5 + 32;
+	fahrenheit.value = parseFloat(output.toFixed(2));
+	console.log(output + "°F");
+
+}
+
+function FarToKel() {
+	let output = ( parseFloat(fahrenheit.value) - 32) * 5/9 + 273.15;
+	kelvin.value = parseFloat(output.toFixed(2));
+	console.log(output + "K");
 }
